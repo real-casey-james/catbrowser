77ffa5974aa92849012c7c3aba319546fd32d336
real-casey-james <caseysjames@gmail.com>
2021-06-06 16:46:58 +1200

    draft working

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var livereload = require("livereload");
-var connectLivereload = require("connect-livereload");
+// var livereload = require("livereload");
+// var connectLivereload = require("connect-livereload");
 const hbs = require('express-handlebars')
 const fs = require('fs')
 
@@ -12,17 +12,17 @@ var indexRouter = require('./routes/index');
 
 const publicDirectory = path.join(__dirname, 'public');
 
-var liveReloadServer = livereload.createServer();
-liveReloadServer.watch(publicDirectory);
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
+// var liveReloadServer = livereload.createServer();
+// liveReloadServer.watch(publicDirectory);
+// liveReloadServer.server.once("connection", () => {
+//   setTimeout(() => {
+//     liveReloadServer.refresh("/");
+//   }, 100);
+// });
 
 var app = express();
 
-app.use(connectLivereload());
+// app.use(connectLivereload());
 
 // Handlebars configuration
 app.engine('hbs', hbs({ extname: 'hbs' }))
@@ -31,7 +31,7 @@ app.set('view engine', 'hbs')
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(publicDirectory));
 
 app.use('/', indexRouter);
