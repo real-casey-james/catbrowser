f4c81b89e131599a2833dbd13979899fcd12981e
real-casey-james <caseysjames@gmail.com>
2021-06-06 16:45:06 +1200

    draft working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var livereload = require("livereload");
 var connectLivereload = require("connect-livereload");
